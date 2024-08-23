71b5ac96847dafb604e1ec0b3e57640ba382be7a
facecjf <6617396+facecjf@users.noreply.github.com>
2024-08-23 11:36:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import MainScene from './js/MainScene.js';
 
 const gameConfig = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 960,
-    height: 540,
+    parent: 'gameContainer',
+    backgroundColor: '#7720a8',
     scale: {
-        mode:  Phaser.Scale.FIT,
-        autoCenter:  Phaser.Scale.CENTER_BOTH
+        mode: Phaser.Scale.FIT,
+        width: 639,
+        height: 1136,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     scene: null
 };
-
 const game = new Phaser.Game(gameConfig);
 
 game.scene.add("Preload", PreloaderScene);
 game.scene.add("Main", MainScene);
 
 game.scene.start("Preload");
-
